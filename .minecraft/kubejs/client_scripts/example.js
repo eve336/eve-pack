8c70dec579dda93212108c34cfc39e6448439b44
eve336 <121055316+eve336@users.noreply.github.com>
2024-11-21 18:59:20 +0000

    small pr

---
+++ .minecraft/kubejs/client_scripts/example.js
@@ -0,0 +1,9 @@
+// priority: 0
+
+// Visit the wiki for more info - https://kubejs.com/
+
+console.info('Hello, World! (Loaded client scripts)')
+
+JEIEvents.hideItems(event => {
+    event.hide(['ae2:facade', 'ae2:vibration_chamber'])
+})
