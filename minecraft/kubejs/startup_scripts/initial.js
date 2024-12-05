ef51a8caa553616d677ec55f34ae397c3e4287a0
eve336 <121055316+eve336@users.noreply.github.com>
2024-12-05 22:45:01 +0000

    nuking stuff

--- minecraft/kubejs/startup_scripts/initial.js
+++ minecraft/kubejs/startup_scripts/initial.js
@@ -4,6 +4,7 @@ global.fluidNukeList = []
 
 ItemEvents.modification(e => {
 
-global.nukeList.push('ad_astra:desh_fluid_pipe', 'ad_astra:ostrum_fluid_pipe','ad_astra:hydrogen_bucket', 'ad_astra:oxygen_bucket', 'rftoolsbase:dimensionalshard')
+global.nukeList.push('ad_astra:desh_fluid_pipe', 'ad_astra:ostrum_fluid_pipe','ad_astra:hydrogen_bucket', 'ad_astra:oxygen_bucket', 'rftoolsbase:dimensionalshard', 'gtceu:solar_panel', 'gtceu:ulv_solar_panel', 'gtceu:lv_solar_panel','gtceu:mv_solar_panel', 'gtceu:hv_solar_panel','gtceu:ev_solar_panel','gtceu:iv_solar_panel', 'gtceu:luv_solar_panel', 'gtceu:zpm_solar_panel','gtceu:uv_solar_panel'
+)
 global.fluidNukeList.push( 'ad_astra:hydrogen', 'ad_astra:oxygen')
 })
