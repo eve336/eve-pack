1bb17f3a3dbdc776a877ed45c22104cd57111d7d
eve336 <121055316+eve336@users.noreply.github.com>
2024-12-21 17:32:49 +0000

    update examplemod

--- minecraft/kubejs/startup_scripts/initial.js
+++ minecraft/kubejs/startup_scripts/initial.js
@@ -7,9 +7,8 @@ ItemEvents.modification(e => {
             'gtceu:solar_panel', 'gtceu:ulv_solar_panel', 'gtceu:lv_solar_panel', 'gtceu:mv_solar_panel', 'gtceu:hv_solar_panel', 'gtceu:ev_solar_panel', 'gtceu:iv_solar_panel',
             'gtceu:luv_solar_panel', 'gtceu:zpm_solar_panel', 'gtceu:uv_solar_panel', 'ad_astra:steel_rod', 'ad_astra:iron_rod', 'ad_astra:steel_block', 'ad_astra:steel_plate',
             'ad_astra:cryo_fuel_bucket', 'ad_astra:oil_bucket', 'ad_astra:fuel_bucket', 'ad_astra:iron_plate', 'functionalstorage:gold_upgrade', 'functionalstorage:diamond_upgrade',
-            'functionalstorage:netherite_upgrade', 'functionalstorage:max_storage_upgrade', 'functionalstorage:creative_vending_upgrade', 'rftoolsbase:machine_infuser', 'rftoolsbase:dimensionalshard_overworld',
-            'rftoolsbase:dimensionalshard_nether', 'rftoolsbase:dimensionalshard_end', 'ironfurnaces:million_furnace', 'ironfurnaces:rainbow_plating', 'ironfurnaces:rainbow_core', 'ironfurnaces:rainbow_coal',
-            'ironfurnaces:augment_generator','ironfurnaces:crystal_furnace'
+            'functionalstorage:netherite_upgrade', 'functionalstorage:max_storage_upgrade', 'functionalstorage:creative_vending_upgrade', 'rftoolsbase:machine_infuser', 'ironfurnaces:million_furnace', 'ironfurnaces:rainbow_plating', 'ironfurnaces:rainbow_core', 'ironfurnaces:rainbow_coal',
+            'ironfurnaces:augment_generator','rftoolsbase:dimensionalshard_overworld', 'rftoolsbase:dimensionalshard_nether', 'rftoolsbase:dimensionalshard_end', 'ironfurnaces:crystal_furnace'
       )
 
 })
