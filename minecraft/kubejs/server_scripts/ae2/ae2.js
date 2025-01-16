7526cc385b9b65cc3ee7d2eae4d541aa5e9f98b2
eve336 <121055316+eve336@users.noreply.github.com>
2025-01-16 21:26:20 +0000

    earlygame ae2 stuff

--- minecraft/kubejs/server_scripts/ae2/ae2.js
+++ minecraft/kubejs/server_scripts/ae2/ae2.js
@@ -10,8 +10,8 @@ ServerEvents.recipes(e => {
     e.remove({id: 'ae2:network/cables/glass_fluix'})
     e.remove({id: 'ae2:network/blocks/pattern_providers_interface'})
     e.remove({id: 'ae2:network/blocks/interfaces_interface'})
-    e.remove({id: 'ae2:network/crafting/patterns_blank'})
     e.remove({id: 'ae2:network/blocks/crystal_processing_growth_accelerator'})
+    e.remove({id: 'ae2:network/blocks/crafting/molecular_assembler'})
 
 
 
@@ -31,6 +31,20 @@ ServerEvents.recipes(e => {
       )
 
       e.shaped(
+        Item.of('ae2:molecular_assembler', 1), // arg 1: output
+        [
+          'IGI',
+          'GCG', // arg 2: the shape (array of strings)
+          'IGI'
+        ],
+        {
+          I: 'minecraft:iron_ingot',
+          G: 'ae2:quartz_glass', 
+          C: 'minecraft:crafting_table'
+        }
+      )
+
+      e.shaped(
         Item.of('ae2:growth_accelerator', 1), // arg 1: output
         [
           'GCG',
@@ -43,5 +57,14 @@ ServerEvents.recipes(e => {
           C: '#gtceu:circuits/lv'
         
         }
+      )
+
+      e.shapeless(
+        Item.of('ae2:fluix_glass_cable', 4),
+        [
+          'ae2:quartz_fiber',
+          'minecraft:quartz',
+          'minecraft:redstone'
+        ]
       )
 })  
